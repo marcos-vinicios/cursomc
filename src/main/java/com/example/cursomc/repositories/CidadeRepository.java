2f30561179bd5dd7fda56d4a4a7f074d09da08e8
marcos-vinicios <marcosviniciosj2@gmail.com>
2021-03-29 22:53:00 -0300

    Estado e cidade

---
+++ src/main/java/com/example/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,13 @@
+package com.example.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+	
+	
+}
