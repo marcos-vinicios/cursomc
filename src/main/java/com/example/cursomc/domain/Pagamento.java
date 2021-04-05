7a0468b96a11a5d730e3deacc9ad02dedb308cc1
marcos-vinicios <marcosviniciosj2@gmail.com>
2021-04-05 20:11:46 -0300

    /pedido{id}

--- src/main/java/com/example/cursomc/domain/Pagamento.java
+++ src/main/java/com/example/cursomc/domain/Pagamento.java
@@ -11,6 +11,7 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.example.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Pagamento implements Serializable {
@@ -19,6 +20,8 @@ public abstract class Pagamento implements Serializable {
 	@Id
 	private Integer id;
 	private Integer estado;
+	
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
