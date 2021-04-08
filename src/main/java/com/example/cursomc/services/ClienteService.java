aef7d5f93802c48ee817c521b9a9b0f12e2f8705
marcos-vinicios <marcosviniciosj2@gmail.com>
2021-04-08 15:57:11 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/example/cursomc/services/ClienteService.java
+++ src/main/java/com/example/cursomc/services/ClienteService.java
@@ -9,10 +9,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.domain.Cidade;
 import com.example.cursomc.domain.Cliente;
+import com.example.cursomc.domain.Endereco;
+import com.example.cursomc.domain.enums.TipoCliente;
 import com.example.cursomc.dto.ClienteDTO;
+import com.example.cursomc.dto.ClienteNewDTO;
+import com.example.cursomc.repositories.CidadeRepository;
 import com.example.cursomc.repositories.ClienteRepository;
+import com.example.cursomc.repositories.EnderecoRepository;
 import com.example.cursomc.services.exceptions.DataIntegrityException;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -23,6 +31,12 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	/*public Cliente buscar(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElse(null);
@@ -31,6 +45,14 @@ public class ClienteService {
 	public Cliente find(Integer id) throws ObjectNotFoundException {
 	Optional<Cliente> obj = repo.findById(id);
 	return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id " + id + " Tipo: " + Cliente.class.getName()));
+	}
+	
+	//@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) throws ObjectNotFoundException {
@@ -61,6 +83,26 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(),objDto.getEmail(), null, null);
 	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cliente = new Cliente(null, objDto.getNome(),objDto.getEmail(),objDto.getCpfOuCnpj(),TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(),null,null);
+		//Cidade cid = cidadeRepository.findOne(objDto.getCidadeId());
+		Endereco endereco = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(),objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cliente,cid);
+		cliente.getEnderecos().add(endereco);
+		
+		cliente.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2()!=null) {
+			cliente.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3()!=null) {
+			cliente.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cliente;
+	}
+	
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
