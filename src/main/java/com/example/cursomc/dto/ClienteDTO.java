a30bfb19312b198e00bdba37cb344090e04ad687
marcos-vinicios <marcosviniciosj2@gmail.com>
2021-04-08 11:32:47 -0300

    Put Delete e Get para clientes

--- src/main/java/com/example/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/example/cursomc/dto/ClienteDTO.java
@@ -2,34 +2,39 @@ package com.example.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.example.cursomc.domain.Cliente;
 
-import com.example.cursomc.domain.Categoria;
-
-public class CategoriaDTO implements Serializable {
+public class ClienteDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
-	
 	@NotEmpty(message = "Preencimento obrigatório!")
-	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	@NotEmpty(message = "Preencimento obrigatório!")
+	@Email(message = "Email inválido!")
+	private String email;
 	
-	public CategoriaDTO() {
+	public ClienteDTO() {
 		
 	}
-	public CategoriaDTO(Categoria obj) {
+	
+public ClienteDTO(Cliente obj) {
 		id = obj.getId();
 		nome = obj.getNome();
+		email = obj.getEmail();
 	}
 
 	public Integer getId() {
 		return id;
 	}
+
 
 	public void setId(Integer id) {
 		this.id = id;
@@ -42,6 +47,15 @@ public class CategoriaDTO implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 	
+
 }
--- src/main/java/com/example/cursomc/services/ClienteService.java
+++ src/main/java/com/example/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.example.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.example.cursomc.domain.Cliente;
+import com.example.cursomc.dto.ClienteDTO;
 import com.example.cursomc.repositories.ClienteRepository;
+import com.example.cursomc.services.exceptions.DataIntegrityException;
 
 import javassist.tools.rmi.ObjectNotFoundException;
 
@@ -16,8 +23,8 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
-	/*public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
+	/*public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElse(null);
 	}
 	*/
@@ -25,5 +32,38 @@ public class ClienteService {
 	Optional<Cliente> obj = repo.findById(id);
 	return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id " + id + " Tipo: " + Cliente.class.getName()));
 	}
-
+	
+	public Cliente update(Cliente obj) throws ObjectNotFoundException {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	public Void delete(Integer id) throws ObjectNotFoundException{
+		try {
+			find(id);
+			repo.deleteById(id);
+			return null;
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir porque a entidades relacionadas.");
+		}
+		
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String direction, String orderBy){
+		//PageRequest pageRequest = new PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+	}
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(),objDto.getEmail(), null, null);
+	}
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+		
+	}
 }
